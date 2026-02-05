f95174b2861d49c6fe815d948b9af26c4ec4b779
HB9HIL <fabian.berg@hb9hil.org>
2026-02-05 23:53:00 +0100

    drop all migrations from 1-170 (170 got replaced by a placeholder for legacy support)

---
+++ application/migrations/170_welcome_to_wavelog.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+*   Just an empty placeholder. To keep compatibility for the migration from Cloudlog to Wavelog we need an empty migration here as placeholder.
+*   Find the tutorial here: https://github.com/wavelog/Wavelog/wiki/How-to-migrate-Cloudlog-to-Wavelog
+*/
+
+class Migration_welcome_to_wavelog extends CI_Migration {
+
+    public function up()
+    {
+    
+        // Nothing happening here
+
+    }
+
+    public function down()
+    {
+        // No rollback available. This is just a placeholder.
+    }
+}
