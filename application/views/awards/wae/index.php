9cab9718882aa69deee639d613625f884afb33c5
phl0 <github@florian-wolters.de>
2026-03-05 09:04:47 +0100

    Revert "Added JS for band changes"
    This reverts commit fe7f7fa9ff1936e972177a3a230121ddde4e5fac.

--- application/views/awards/wae/index.php
+++ application/views/awards/wae/index.php
@@ -8,27 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		});
 	});
 });
-$('#band2').change(function(){
-   var band = $("#band2 option:selected").text();
-   if (band != "SAT") {
-      $("#sats").val('All');
-      $("#orbits").val('All');
-      $("#satrow").hide();
-      $("#orbitrow").hide();
-   } else {
-      $("#satrow").show();
-      $("#orbitrow").show();
-   }
-});
-
-$('#band2').change();	// trigger the change on fresh-load to hide/show SAT-Params
-
-$('#sats').change(function(){
-   var sat = $("#sats option:selected").text();
-      $("#band2").val('SAT');
-   if (sat != "All") {
-   }
-});
 </script>
 <div class="container">
         <!-- Award Info Box -->
